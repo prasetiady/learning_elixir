3ddbcc88fa368f45deac786c7685b8b376cf22db
Dedy Prasetiady <dedy.berastagi@gmail.com>
2017-02-02 14:51:47 +0700

    Learning

--- first_project/sequence/lib/sequence.ex
+++ first_project/sequence/lib/sequence.ex
@@ -1,18 +1,7 @@
 defmodule Sequence do
-  @moduledoc """
-  Documentation for Sequence.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sequence.hello
-      :world
-
-  """
-  def hello do
-    :world
+  use Application
+  
+  def start(_type, _args) do
+    {:ok, _pid} = Sequence.Supervisor.start_link(123)
   end
 end
